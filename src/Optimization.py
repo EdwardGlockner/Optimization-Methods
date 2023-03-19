929fbea1f6b01998620c96dbbf9693f41b3c3f26
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-03-19 10:54:53 +0100

    bugs

---
+++ src/Optimization.py
@@ -0,0 +1,13 @@
+"""
+
+
+"""
+
+class Optimization():
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+        pass
+
+    def Gradient_descent(self, guess, f, df, tolerance)
+
+
